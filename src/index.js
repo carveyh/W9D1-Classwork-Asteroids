d75d31259d53aee9995b6c1270bd756a0b45f47b
jliangrpb <jeremy.liang612@gmail.com>
2023-05-01 17:25:49 -0400

    ALmost got the circle :c

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 console.log('Webpack is working');
-
 import MovingObject from "./moving_object.js";
+window.MovingObject = MovingObject;
+
+// console.log('hi')
 
 // import MovingObject from "./moving_object.js";
 
-window.MovingObject = MovingObject;
+
+const mo = new MovingObject({
+  pos: [30, 30],
+  vel: [10, 10],
+  radius: 5,
+  color: "#00FF00"
+});
+// console.log('hi')
+document.addEventListener('DOMContentLoaded', () => {
+
+    const canvasEl = document.getElementById('game-canvas');
+    const ctx = canvasEl.getContext("2d");
+    // console.log('hi')
+    
+    mo.draw(ctx);
+})
--- src/moving_object.js
+++ src/moving_object.js
@@ -5,7 +5,17 @@ class MovingObject {
         this.radius = options['radius'];
         this.color = options['color'];
     }
+
+    draw (ctx) {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(...this.pos, 0, 2*Math.PI, false);
+        ctx.strokeStyle = 'orange';
+        ctx.lineWidth = 20;
+        ctx.stroke();
+        ctx.fill();
+        // console.log('ha')
+    };
 }
 
 export default MovingObject; 
-// export default MovingObject;
